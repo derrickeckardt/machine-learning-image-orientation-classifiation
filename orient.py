033cfa80b1288b1b5ef5ac75294bc7eef3146a1b
derrick eckardt <derrick.eckardt@gmail.com>
2018-12-03 04:01:14 +0000

    Working on my trees

--- orient.py
+++ orient.py
@@ -34,6 +34,7 @@ from operator import itemgetter
 from collections import Counter, deque
 import profile
 import numpy as np
+import pandas as pd
 from scipy.spatial.distance import euclidean as euclid
 from math import pow
 from numpy import square
@@ -50,6 +51,50 @@ def import_images(input_file):
             # List of list, with format of [train_image, orientation, [rgb values]]
             training_images.extend([[splitline[0][6:], int(splitline[1]), [int(value) for value in splitline[2:]]]])
     return training_images
+    
+def import_for_trees(input_file):
+    # create headings
+    pixels = [str(i) for i in range(1,9)]
+    features =['filename','orientation'] + [color+row+col for row in pixels for col in pixels for color in 'rgb' ]
+
+    # Load into a pandas dataframe, since that will make sorting by features easiest
+    training_images = pd.read_csv(input_file, names=features, sep=" ")
+    
+    # drop filename column
+    training_images['filename'].drop
+    
+    # features doesn't need to track filename or orientation
+    features = features[2:]
+
+    return training_images, features
+
+def dt_filters(training_images):
+    
+    # Make a copy
+    filtered_images = training_images
+
+    # Various filters I have used
+    # Convert them to dark or light (0 or 1)
+    def halves(x):
+        return 0 if x <128 else 1
+
+    # Converts them to quarters, from 0, 1, 2, or 3
+    def quarters(x):
+        return 0 if x <64 else 1 if x < 128 else 2 if x < 192 else 3
+
+    # apply filters
+    for column in features:
+        filtered_images[column] = filtered_images[column].apply(quarters)
+
+    return filtered_images
+
+def entropy(training_images,eval_column):
+    items = np.unique(filtered_images[eval_column])
+    counts = Counter(filtered_images[eval_column])
+    total_items = float(sum(counts.values()))
+    entropy = sum([(-counts[each]/total_items)*np.log2(counts[each]/total_items) for each in items])
+    return entropy
+    
 
 # For all take a list of list with inputs in form of [image, guess_orientation, actual_orientation]
 def output(results):
@@ -68,8 +113,7 @@ def output(results):
     output_file.close
     print "Individual test cases outputted to 'output.txt'."
         
-# Use within nearest()
-# Not currently used, as found it slightly faster to embed formula within code
+# Use within nearest() - Not currently used, as found it slightly faster to use dictionaries
 def euclidean(train_features,test_features):
     feature_range = range(len(train_features))
     return sum([(train_features[i]-test_features[i])**2 for i in feature_range])
@@ -91,9 +135,8 @@ def nearest_test(train_file, test_file, k):
     print "Classifying %d images. Estimated runtime is %d minutes and %.0f seconds." % (len(test_images) , (len(test_images) / 150), ((len(test_images) % 150) * (60.0/150.0)))
     feature_range, results = range(len(train_images[0][2])), []
     # create dictionary of squares, save computation time later.
-    # takes a lot of time to keep finding euclidean distances.
-    euclid_dict = {}
-    euc_range = range(-256,256)
+    # takes a lot of time to keep finding the squares for the euclidean distances.
+    euclid_dict, euc_range = {}, range(-256,256)
     # euc_range = range(0,256)
     for eu in euc_range:
         euclid_dict[eu] = eu**2
@@ -145,27 +188,31 @@ def nearest_test(train_file, test_file, k):
         #         print "interim", distances
         # print "final  ",distances
         vote_guess = Counter([vote[1] for vote in distances]).most_common(1)[0][0]
-        results.extend([[test_image, distances, actual_orientation]])  #vote_guess
-    # back-up copy
-    # for test_image, actual_orientation, test in test_images:
-    #     distances = []
-    #     for image, orientation, train in train_images:
-    #         euclidean = 0 # This was actually the fastest way
-    #         for j in feature_range:
-    #             euclidean += (train[j]-test[j])**2  # Don't need to find square root, since relative, save the operation
-    #         # euclidean = np.cumsum([(train[j]-test[j])**2 for j in feature_range])[-1]
-    #         # print euclidean
-    #         # euclidean = euclid(train,test)
-    #         distances.extend([[image, orientation, euclidean]])
-    #     vote_guess = Counter([vote[1] for vote in sorted(distances, key=itemgetter(2))[:k]]).most_common(1)[0][0]
-    #     results.extend([[test_image, vote_guess, actual_orientation]])
+        results.extend([[test_image, vote_guess, actual_orientation]])  #vote_guess
 
     return results
     
 if traintest == "train":
     # Import train file
     if model == "nearest":
+        print "Training via k-Nearest Neighbors algorithm."
         nearest_train()
+    elif model =="forest":
+        print "Training via Forest algorithm."
+        print "Loading images from '"+input_file+"'."
+        training_images, features = import_for_trees(input_file)
+        print "Running filters to make features friendlier."
+        filtered_images = dt_filters(training_images)
+        entropies = [[feature, entropy(filtered_images,feature)] for feature in features ]
+        max_ent = sorted(entropies, key=itemgetter(1), reverse=True)[0]
+        # make root
+            # recursively call it
+            # reduce feature set
+            # determine how far down to go
+            
+        
+        print max_ent
+        print sorted(entropies, key=itemgetter(1), reverse=True)[-1]
     else:
         print "Unsupported Machine Learning Model."
 
@@ -177,7 +224,7 @@ elif traintest == "test":
         results = nearest_test(model_file, input_file, k)
 
         # Testing code to find slowest part of code
-        profile.run("nearest_test(model_file,input_file, 11)")
+        # profile.run("nearest_test(model_file,input_file, 11)")
     else:
         print "Unsupported Machine Learning Model."
 
@@ -194,8 +241,7 @@ else:
 def adaboost():
     pass
 
-def entropy():
-    pass
+
 
 def forest():
     pass
